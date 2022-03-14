d7371f7eba758674ea89c84684739f37de578eb4
skabanakis <85350624+skabanakis@users.noreply.github.com>
2022-03-14 12:08:50 +0000

    added assignment

--- sentence_generation/weather_generator.py
+++ sentence_generation/weather_generator.py
@@ -1,7 +1,9 @@
 import random
 weather_chain = {
     'sun': ['sun', 'sun', 'sun', 'sun', 'sun', 'sun', 'sun', 'sun', 'sun', 'rain'],
-    'rain': ['sun', 'rain']
+    'rain': ['sun', 'rain','snow'],
+    'snow':['sun', 'rain', 'snow','cloudy'],
+    'cloudy':['sun','rain','snow','cloudy','sun']
 }
 
 weather = [random.choice(list(weather_chain.keys()))]
